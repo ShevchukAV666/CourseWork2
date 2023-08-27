c859ee340d6960c40e386c971de2b1a320285dab
ShevchukAV666 <666mog@mail.ru>
2023-08-27 17:36:51 +0500

    Коммит

---
+++ src/test/java/Course/Work2/CourseWork2/CourseWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package Course.Work2.CourseWork2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
